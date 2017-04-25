ae38314430d4489ef817e67af562f1e510931574
alder <alder@10.130.222.169>
2017-04-24 19:03:09 -0600

    Pre-champs push 4/24

--- src/org/usfirst/frc/team2220/robot/commands/GearCameraLineUp.java
+++ src/org/usfirst/frc/team2220/robot/commands/GearCameraLineUp.java
@@ -45,7 +45,7 @@ public class GearCameraLineUp extends Command
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished()
 	{
-		return TankDrive.getInstance().hasHitRSetpoint() && TankDrive.getInstance().hasHitLSetpoint();
+		return TankDrive.getInstance().hasHitRSetpoint() && TankDrive.getInstance().hasHitLSetpoint(); //TODO why does this exit early?
 	}
 
 	// Called once after isFinished returns true
